70bf94a8a42371d3718636b659886c199a1cc121
unknown <nick.chen.1686@gmail.com>
2023-06-06 22:11:21 +0800

    command - ./mvnw install

---
+++ src/main/java/com/example/springtutorialangular/SpringTutorialAngularApplication.java
@@ -0,0 +1,13 @@
+package com.example.springtutorialangular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTutorialAngularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTutorialAngularApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springtutorialangular/SpringTutorialAngularApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springtutorialangular;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTutorialAngularApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
