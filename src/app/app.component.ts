95a7726060c4cfdb43e8f518be5de35195615170
unknown <nick.chen.1686@gmail.com>
2023-06-07 11:06:01 +0800

    add dynamic content

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'client';
+  title = 'Demo';
+  greeting = {};
+  constructor(private http: HttpClient) {
+    http.get<any>('resource').subscribe((data: { id: String, content: String }) => this.greeting = data);
+  }
 }
